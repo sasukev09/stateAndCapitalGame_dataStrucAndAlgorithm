ffcfcfc7b0e75ef9a8eaafa97fe23a45745a7565
sasukev09 <ksala83@wgu.edu>
2023-12-19 12:59:33 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.sql.Array;
+import java.util.Scanner;
+import java.lang.String;
+
+public class Main {
+    public static void main(String[] args) {
+        //creating a two dimensional array for
+        String[][] stateAndCapital = {{"Alabama", "Alaska", "Arizona", "Arkansas",
+                "California", "Colorado", "Connecticut", "Delaware", "Florida",
+                "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+                "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
+                "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska",
+                "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina",
+                "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
+                "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+                "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"},
+                {"Montgomery", "Juneau", "Phoenix", "Little Rock", "Sacramento", "Denver", "Hartford", "Dover", "Tallahassee", "Atlanta", "Honolulu", "Boise", "Springfield", "Indianapolis", "Des Moines", "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis", "Boston", "Lansing", "Saint Paul", "Jackson", "Jefferson City", "Helena", "Lincoln", "Carson City", "Concord", "Trenton", "Santa Fe", "Albany", "Raleigh", "Bismarck", "Columbus", "Oklahoma City", "Salem", "Harrisburg", "Providence", "Columbia", "Pierre", "Nashville", "Austin", "Salt Lake City", "Montpelier", "Richmond", "Olympia", "Charleston", "Madison", "Cheyenne"}
+        };
+
+        Scanner inputScanner  = new Scanner(System.in);
+
+    }
+}
